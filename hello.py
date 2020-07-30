3effe72bfdd30dbc7be4a2ddc6f2c2d8378bc7db
yeshwanth surya <yeshsurya@gmail.com>
2020-07-30 14:15:26 +0530

    Remove ssl

--- hello.py
+++ hello.py
@@ -250,4 +250,4 @@ def shutdown():
         client.disconnect()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port, debug=True,ssl_context='adhoc')
+    app.run(host='0.0.0.0', port=port, debug=True)
